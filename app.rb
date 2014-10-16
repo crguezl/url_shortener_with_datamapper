97724e4dc96fbd520be9bce19bba82a41e6a9ff0
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-10-16 09:08:20 +0100

    separating model in model.rb

--- app.rb
+++ app.rb
@@ -5,33 +5,10 @@ require 'sinatra/reloader' if development?
 require 'haml'
 require 'uri'
 require 'data_mapper'
+require_relative 'model'
 require 'pp'
 
 require 'socket'
-
-DataMapper.setup( :default, ENV['DATABASE_URL'] || 
-                            "sqlite3://#{Dir.pwd}/my_shortened_urls.db" )
-DataMapper::Logger.new($stdout, :debug)
-DataMapper::Model.raise_on_save_failure = true 
-
-class ShortenedUrl
-  include DataMapper::Resource
-
-  property :id, Serial
-  property :url, Text
-end
-
-DataMapper.finalize
-require  'dm-migrations'
-
-# This will issue the necessary CREATE statements (DROPing the table
-#first, if it exists) to define each storage according to their
-#properties.
-# After auto_migrate! has been run, the database should be in a pristine state.
-# All the tables will be empty and match the model definitions.
-#DataMapper.auto_migrate!
-DataMapper.auto_upgrade!
-#set :address, address # 'http://localhost:9292'
 
 get '/' do
   puts "inside get '/': #{params}"
