170afd8dca46c6bfe3d796f5b23c5495e9e9c571
jonathan1985 <alu0100450187@ull.edu.es>
2014-10-23 16:04:07 +0100

    cambios menores

--- test/test.rb
+++ test/test.rb
@@ -2,7 +2,7 @@ ENV['RACK_ENV'] = 'test'
 
 require 'minitest/autorun'
 require 'rack/test'
-require_relative '../twitter.rb'
+require_relative '../app.rb'
 
 
 include Rack::Test::Methods
@@ -11,30 +11,9 @@ def app
     Sinatra::Application
 end
 
-describe "SYTW P2 - page" do
+describe "Acortador  - page" do
     
     
-    it "Should return index" do
-        get '/'
-        assert last_response.ok?
-    end
-    
-    it "should return title" do
-        get '/'
-        assert_match "<title>SYTW - P2 - Amigos Populares en Twitter</title>", last_response.body
-    end
-    
-    it "should return form" do
-        get '/'
-        assert_match "<p ><b>Introduzca su nombre en Twitter: </b></p>", last_response.body
-        assert_match "<p ><b>¿Cuántos seguidores desea ver? </b></p>", last_response.body
-    end
-
-
-    it "Debe cargar la imagen del pajarito" do
-	get '/'
-	assert_match '<img src="/bird_icon.png" WIDTH=100 HEIGHT=100 />', last_response.body
-    end
 
     it "should return foot" do
         get '/'
@@ -47,19 +26,3 @@ describe "SYTW P2 - page" do
     #end
 end
 
-describe "SYTW P2 - Funtions" do
-    before :all do
-        @tw = Twitterpopular.new
-        @client = my_twitter_client()
-        @name1 = "b0nedark"
-        @name2 = "bosdadanedark"
-    end
-    
-    it "the user exist" do
-        assert @tw.dusuario(@client,@name1)
-    end
-    
-    it "the user not exist" do
-        refute @tw.dusuario(@client, @name2)
-    end
-end
